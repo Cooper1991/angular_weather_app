94ecfd6cfb98499e81c16156ef0295297b235498
Jack Cooper <JCooper@GRAPEVINE.local>
2016-05-24 10:29:57 +0100

    seperated angular components

--- app.js
+++ app.js
@@ -3,69 +3,6 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 
-//CONFIG TO TEMPLATES DEPENDING ON HASH VALUE
-weatherApp.config(function($routeProvider){
-    
-    $routeProvider
-    
-     .when('/',{
-        templateUrl:'pages/home.html',
-        controller:'homeController'
-    })
-    
-    .when('/forecast',{
-        templateUrl:'pages/forecast.html',
-        controller:'forecastController'
-    })
-    
-})
-
-
-
-
-//SERVICES
-weatherApp.service('cityService', function(){
-    
-    this.city = 'New York, NY';
-    
-})
-
-
-
-
-//CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
-    
-    //City for this scope is equal to the city in the cityService service
-    $scope.city = cityService.city;
-    
-    //Watch the scope for this on cityService. So when cityService city changes, so will this values
-    //Now we will use ng-model="city" in our html - this is the directive for two way data binding
-    $scope.$watch('city', function(){
-        cityService.city = $scope.city;
-    })
-    
-}]);
-
-
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
-    
-    
-    $scope.city = cityService.city;
-    
-    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',{
-        //This basically tells the app that it's okay to grab the JSON from an online source. You can trust it and it's not an attempt at a hack
-        callback : 'JSON_CALLBACK'
-    }, {get:{method:'JSONP'}});
-        
-    $scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:2});
-    
-    console.log($scope);
-        
-    
-    
-}]);
-
 
 //http://api.openweathermap.org/data/2.5/forecast/daily?APPID=14177fc184afb4b3a26bb6de5abdf62b
 
---
+++ controllers.js
@@ -0,0 +1,43 @@
+
+//CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+    
+    //City for this scope is equal to the city in the cityService service
+    $scope.city = cityService.city;
+    
+    //Watch the scope for this on cityService. So when cityService city changes, so will this values
+    //Now we will use ng-model="city" in our html - this is the directive for two way data binding
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    })
+    
+}]);
+
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
+    
+    
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=14177fc184afb4b3a26bb6de5abdf62b',{
+        //This basically tells the app that it's okay to grab the JSON from an online source. You can trust it and it's not an attempt at a hack
+        callback : 'JSON_CALLBACK'
+    }, {get:{method:'JSONP'}});
+        
+    $scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:$scope.days});
+    
+    $scope.convertToFahrenheit = function(degK){
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+    }
+    
+    $scope.convertToDate = function(dt){
+        
+        return new Date(dt * 1000);
+    }
+        
+ 
+    
+}]);
